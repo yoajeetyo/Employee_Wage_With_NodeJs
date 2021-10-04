4695b5c5c6d06b92059f2f758b0ff3dfb55d9cf1
yoajeetyo <sajeetkumar40@gmail.com>
2021-10-04 16:48:06 +0530

    Added: calculating wage for part time , full time and for absent for one day

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,8 +1,23 @@
 console.log("Welcome to Employee wage program")
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random()*10)%2;
-if (empCheck==IS_ABSENT) {
-    console.log("Employee is Absent!!!");
-} else {
-    console.log("Employee is Present!!");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOUR = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR = 20;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+switch(empCheck) {
+case IS_PART_TIME :
+    empHrs = PART_TIME_HOUR;
+    break;
+case IS_FULL_TIME :
+    empHrs = FULL_TIME_HOUR;
+    break;
+default :
+empHrs = 0;
+break;
+
 }
+
+let empWage = empHrs*WAGE_PER_HOUR;
+console.log("Employee wage is : "+empWage);
