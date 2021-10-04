063af8f73be404ca5c9953d62a32501bbc3e1c4a
yoajeetyo <sajeetkumar40@gmail.com>
2021-10-04 17:55:10 +0530

    Added : Define a function to check employee hours and calculate wages

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,20 +4,18 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
-let empHrs = 0;
-let empCheck = Math.floor(Math.random()*10)%3;
+
+function getWorkingHours(empCheck) {
 switch(empCheck) {
 case IS_PART_TIME :
-    empHrs = PART_TIME_HOUR;
-    break;
+    return PART_TIME_HOUR;
 case IS_FULL_TIME :
-    empHrs = FULL_TIME_HOUR;
-    break;
+    return FULL_TIME_HOUR;
 default :
-empHrs = 0;
-break;
-
+    return 0;
 }
-
+}
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkingHours(empCheck);
 let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Employee wage is : "+empWage);
+console.log("Employee Hour : "+empHrs+" Employee wage is : "+empWage);
